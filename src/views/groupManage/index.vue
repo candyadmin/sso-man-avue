842e39a3a413cb0b56aa108a2d1fea6999680562
candyadmin <193661405@qq.com>
2019-04-01 10:54:37 +0800

    first commit

---
+++ src/views/groupManage/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <avue-crud :data="data" :option="option" >
+    <template slot-scope="props" slot="expand">
+      <el-tag>当前行的{{props}}</el-tag>
+    </template>
+  </avue-crud>
+
+</template>
+
+<script>
+  import data from '@/const/aueStudy/data.js'
+  export default {
+    data() {
+      return data;
+    },
+    methods: {
+      expandChange(row, expendList) {
+        this.$message.success('展开回调')
+      }
+    }
+  }
+</script>
+
